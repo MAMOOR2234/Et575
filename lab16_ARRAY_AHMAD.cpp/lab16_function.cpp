19c3c3421e2df9c6b90a111d0ddb9d54932c61fa
MAMOOR2234 <139732821+MAMOOR2234@users.noreply.github.com>
2025-11-18 18:44:47 -0500

    Fix sum calculation for even numbers

--- lab16_ARRAY_AHMAD.cpp/lab16_function.cpp
+++ lab16_ARRAY_AHMAD.cpp/lab16_function.cpp
@@ -74,7 +74,7 @@ int sum_even_numbers(const int *arr, int sizearray){
     int sum = 0;
     for (int i = 0; i < sizearray; i++){
         if (arr[i] % 2 == 0){
-            sum += arr[i];
+            sum = sum +  arr[i];
         }
     }
     return sum;
